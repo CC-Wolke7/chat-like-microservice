bb71332e00190b66f50d774cdd37971d443a3500
Nik Sauer <nik.sauer@me.com>
2021-01-13 16:22:03 +0100

    Simplify app gateway

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,36 +1,23 @@
 import {
   SubscribeMessage,
   WebSocketGateway,
-  WsResponse,
-  WebSocketServer,
+  ConnectedSocket,
 } from '@nestjs/websockets';
-import { Server } from 'ws';
 import { AppEvent } from './event';
 import { HealthStatus } from './interfaces/health';
+import * as WebSocket from 'ws';
 
 @WebSocketGateway()
 export class AppGateway {
-  // MARK: - Private Properties
-  @WebSocketServer()
-  private readonly server: Server;
-
   // MARK: - Public Methods
   // MARK: Event Handler
   @SubscribeMessage(AppEvent.HealthRequest)
-  getHealth(): void {
-    // @TODO: return class instance conforming to `WsResponse` interface
-    // to support `ClassSerializerInterceptor`
-
-    this.broadcast({ event: AppEvent.HealthStatus, data: HealthStatus.Normal });
-
-    // WsResponse<HealthStatus>
-    // return { event: AppEvent.HealthStatus, data: HealthStatus.Normal };
-  }
-
-  // MARK: - Private Methods
-  private broadcast(payload: WsResponse): void {
-    for (const client of this.server.clients) {
-      client.send(JSON.stringify(payload));
-    }
+  getHealth(@ConnectedSocket() socket: WebSocket): void {
+    socket.send(
+      JSON.stringify({
+        event: AppEvent.HealthStatus,
+        data: HealthStatus.Normal,
+      }),
+    );
   }
 }
